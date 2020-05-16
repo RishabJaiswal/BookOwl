6c22ff32d536e45e1ec2867e163cec6113efa137
Rishab <shivam.jaiswal175@gmail.com>
2020-05-16 18:04:52 +0530

    created book activity with basic views added for genre title and search View

---
+++ app/src/main/java/com/gutenberg/bookowl/view/books/BooksActivity.kt
@@ -0,0 +1,13 @@
+package com.gutenberg.bookowl.view.books
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gutenberg.bookowl.R
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
