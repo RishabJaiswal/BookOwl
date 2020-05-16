d4215866a5d368ee88aa7bf67d29b745a7cba304
Rishab <shivam.jaiswal175@gmail.com>
2020-05-17 01:55:49 +0530

    added mime types

--- app/src/main/java/com/gutenberg/bookowl/application/Constants.kt
+++ app/src/main/java/com/gutenberg/bookowl/application/Constants.kt
@@ -1,3 +1,11 @@
 package com.gutenberg.bookowl.application
 
 const val KEY_GENRE = "genre"
+
+//mimetypes
+const val MIME_IMAGE = "image/jpeg"
+const val MIME_PDF = "application/pdf"
+const val MIME_TEXT_PLAIN_ASCII = "text/plain; charset=us-ascii"
+const val MIME_TEXT_PLAIN_UTF_8 = "text/plain; charset=utf-8"
+const val MIME_TEXT_HTML_ASCII = "text/html; charset=us-ascii"
+const val MIME_TEXT_HTML_UTF_8 = "text/html; charset=utf-8"
