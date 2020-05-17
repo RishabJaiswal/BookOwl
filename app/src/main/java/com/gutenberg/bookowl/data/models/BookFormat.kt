952c870798ccde9c95cfeb1e6d9362e1f76de6cc
Rishab <shivam.jaiswal175@gmail.com>
2020-05-17 13:43:01 +0530

    fixed book formats serialization

--- app/src/main/java/com/gutenberg/bookowl/data/models/BookFormat.kt
+++ app/src/main/java/com/gutenberg/bookowl/data/models/BookFormat.kt
@@ -1,9 +1,10 @@
 package com.gutenberg.bookowl.data.models
 
-import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.gutenberg.bookowl.application.*
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class BookFormat(
 
     @JsonProperty(MIME_IMAGE)
@@ -13,10 +14,26 @@ data class BookFormat(
     var pdfUrl: String? = "",
 
     @JsonProperty(MIME_TEXT_PLAIN_ASCII)
-    @JsonAlias(MIME_TEXT_PLAIN_UTF_8)
     var plainTextUrl: String? = "",
 
+    @JsonProperty(MIME_TEXT_PLAIN_UTF_8)
+    var plainText_UTF_8_Url: String? = "",
+
     @JsonProperty(MIME_TEXT_HTML_ASCII)
-    @JsonAlias(MIME_TEXT_HTML_UTF_8)
-    var htmlTextUrl: String? = null
-)
+    var htmlTextUrl: String? = null,
+
+    @JsonProperty(MIME_TEXT_HTML_UTF_8)
+    var htmlText_UTF_8_Url: String? = null
+
+) /*{
+
+    @JsonSetter(MIME_TEXT_PLAIN_UTF_8)
+    fun setPlainTextUtf8(value: String){
+        plainTextUrl = value
+    }
+
+    @JsonSetter(MIME_TEXT_HTML_UTF_8)
+    fun setHtmlTextUtf8(value: String) {
+        htmlTextUrl = value
+    }
+}*/
