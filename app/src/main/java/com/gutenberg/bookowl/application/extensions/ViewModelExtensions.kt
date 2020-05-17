8d727ef5fb9bc9ec6913d0d19f697fc8c806c266
Rishab <shivam.jaiswal175@gmail.com>
2020-05-17 22:40:20 +0530

    Fix: ViewModelExtensions

--- app/src/main/java/com/gutenberg/bookowl/application/extensions/ViewModelExtensions.kt
+++ app/src/main/java/com/gutenberg/bookowl/application/extensions/ViewModelExtensions.kt
@@ -4,7 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 
 @Suppress("UNCHECKED_CAST")
 class BaseVMFactory<T>(val block: () -> T) : ViewModelProvider.Factory {
@@ -14,23 +13,17 @@ class BaseVMFactory<T>(val block: () -> T) : ViewModelProvider.Factory {
 }
 
 inline fun <reified T : ViewModel> FragmentActivity.configureViewModel(noinline block: (() -> T)? = null): T {
-    if (block == null) {
-        return ViewModelProviders.of(this).get(T::class.java)
+    return if (block == null) {
+        ViewModelProvider(this).get(T::class.java)
     } else {
-        return ViewModelProviders.of(
-            this,
-            BaseVMFactory(block)
-        ).get(T::class.java)
+        ViewModelProvider(this, BaseVMFactory(block)).get(T::class.java)
     }
 }
 
 inline fun <reified T : ViewModel> Fragment.configureViewModel(noinline block: (() -> T)? = null): T {
-    if (block == null) {
-        return ViewModelProviders.of(this).get(T::class.java)
+    return if (block == null) {
+        ViewModelProvider(this).get(T::class.java)
     } else {
-        return ViewModelProviders.of(
-            this,
-            BaseVMFactory(block)
-        ).get(T::class.java)
+        ViewModelProvider(this, BaseVMFactory(block)).get(T::class.java)
     }
 }
