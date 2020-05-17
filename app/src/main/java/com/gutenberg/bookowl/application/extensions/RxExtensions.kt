b7aff02f56233654f21d4e3f76d04fb47665619b
Rishab <shivam.jaiswal175@gmail.com>
2020-05-17 22:45:33 +0530

    removed unsused methods

--- app/src/main/java/com/gutenberg/bookowl/application/extensions/RxExtensions.kt
+++ app/src/main/java/com/gutenberg/bookowl/application/extensions/RxExtensions.kt
@@ -1,7 +1,5 @@
 package com.gutenberg.bookowl.application.extensions
 
-import io.reactivex.Completable
-import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -14,49 +12,12 @@ fun <T> Observable<T>.subscribeOnBackObserverOnMain(): Observable<T> {
         .observeOn(AndroidSchedulers.mainThread())
 }
 
-fun <T> Flowable<T>.subscribeOnBackObserverOnMain(): Flowable<T> {
-    return this.subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-}
-
 fun <T> Single<T>.subscribeOnBackObserverOnMain(): Single<T> {
     return this.subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-}
-
-fun Completable.subscribeOnBackObserverOnMain(): Completable {
-    return this.subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-}
-
-fun <T> Single<T>.subscribeObserverOnMain(): Single<T> {
-    return this.subscribeOn(AndroidSchedulers.mainThread())
-        .observeOn(AndroidSchedulers.mainThread())
-}
-
-fun <T> Flowable<T>.subscribeObserverOnMain(): Flowable<T> {
-    return this.subscribeOn(AndroidSchedulers.mainThread())
-        .observeOn(AndroidSchedulers.mainThread())
-}
-
-fun Completable.subscribeObserverOnMain(): Completable {
-    return this.subscribeOn(AndroidSchedulers.mainThread())
         .observeOn(AndroidSchedulers.mainThread())
 }
 
 
 fun Disposable.addTo(disposable: CompositeDisposable) {
     disposable.add(this)
-}
-
-fun <T> Single<T>.flatMapResumeAfter(block: (T) -> Completable): Single<T> {
-    return this.flatMap { t ->
-        block(t).andThen(Single.just(t))
-    }
-}
-
-fun <T> Observable<T>.flatMapResumeAfter(block: (T) -> Completable): Observable<T> {
-    return this.flatMap { t ->
-        block(t).andThen(Observable.just(t))
-    }
 }
